de0fecad6b68dd09fb57adc15b68a1fdff1d7635
KazukiMoriya <moriya@aidac.co.jp>
2022-11-01 11:31:12 +0900

    header プロパティ調整

---
+++ assets/_index.js
@@ -0,0 +1,10 @@
+$(function(){
+  $(".js-hamburger").on("click",function(){
+    $(this).children().toggleClass("is-active");
+    if($(".l-header__bar").hasClass("is-active")){
+      $(".l-header-nav").addClass("is-show");
+    }else{
+      $(".l-header-nav").removeClass("is-show");
+    }
+  });
+});
